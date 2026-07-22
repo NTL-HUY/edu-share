781f9bc0643eba84a4dc80e621d412fa5095ec55
NguyenBaoHuy <nguyenbaohuy06@gmail.com>
2026-07-22 09:01:43 +0700

    initial commit

---
+++ edushare-backend/src/main/java/com/nbh/edushare/EdushareApplication.java
@@ -0,0 +1,13 @@
+package com.nbh.edushare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdushareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdushareApplication.class, args);
+	}
+
+}
---
+++ edushare-backend/src/test/java/com/nbh/edushare/EdushareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbh.edushare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdushareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
